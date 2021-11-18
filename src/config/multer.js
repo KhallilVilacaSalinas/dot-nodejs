46d455e1f2ccbe81e07a1673adeb299fd9f7937f
KhallilVSalinas <khallil@tetracon.com.br>
2021-11-18 20:58:01 -0300

    TESTE DOTENV 2

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,11 +1,18 @@
 
-require('dotenv').config();
-const aws = require("aws-sdk");
+const AWS = require("AWS-sdk");
 const multerS3 = require("multer-s3");
 const crypto = require("crypto");
 
+AWS.config.update({
+  region: 'us-east-1',
+  apiVersion: 'latest',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  accessSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+console.log(process.env.AWS_ACCESS_KEY_ID)
 const storageTypes = multerS3({
-  s3: new aws.S3(),
+  s3: new AWS.S3(),
   bucket: 'comprovanteponto',
   contentType: multerS3.AUTO_CONTENT_TYPE,
   acl: 'public-read',
